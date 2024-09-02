90e9bfcbddb3a0b0b1abee7f5f5fa432ccbf9071
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2024-09-01 21:02:22 -0700

    Update MergeStringsAlternatelyStringBuilder.cs

--- 1768 - Merge Strings Alternately/MergeStringsAlternatelyStringBuilder.cs
+++ 1768 - Merge Strings Alternately/MergeStringsAlternatelyStringBuilder.cs
@@ -2,8 +2,7 @@
 
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        StringBuilder sb = new StringBuilder();
-      
+        StringBuilder sb = new StringBuilder(); 
         for (int i = 0; i < word1.Length || i < word2.Length; i++) {
             if (i < word1.Length) {
                 sb.Append(word1[i]);
