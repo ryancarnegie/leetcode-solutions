e4bd35db46f1dac3817637775f2932d9f26c19a1
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-12 18:30:28 -0800

    Update DuplicateBruteForce.py

--- Contains_duplicate/DuplicateBruteForce.py
+++ Contains_duplicate/DuplicateBruteForce.py
@@ -2,11 +2,11 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         n = len(nums)
         for x in range(n):
-            for y in range(n):
+            for y in range(x + 1, n):
                 if nums[x] == nums[y] and x != y:
                     return True
         return False
 
 # Time: O(N^2)
 # Space: O(1)
-        
+# Very inefficient, will exceed time limits in some circumstances
