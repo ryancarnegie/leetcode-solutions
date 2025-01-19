672addce7d0eee1785debc8c5ba87337cc41afc1
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-18 23:33:19 -0800

    Update MaxProfit.py

--- BuyAndSellBestTime/MaxProfit.py
+++ BuyAndSellBestTime/MaxProfit.py
@@ -1,1 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                profit = prices[j] - prices[i]
+                if profit > 0:
+                    max_profit = max(max_profit, profit)
+        return max_profit
 
+# Time : O(N^2) - It's a nested for loop
+# Space : O(1)
