00e9479693f3fa00cfb8a2cb2dc84bf56dc58682
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-25 15:40:14 -0800

    Update BuySellOptimalSolution.py

--- BuyAndSellBestTime/BuySellOptimalSolution.py
+++ BuyAndSellBestTime/BuySellOptimalSolution.py
@@ -4,13 +4,8 @@ class Solution:
         max_profit = 0
 
         for price in prices:
-            if min_price > 0:
-                profit = price - min_price
-                min_price = min(price, min_price)
-                max_profit = max(profit, max_profit)
+            profit = price - min_price
+            min_price = min(price, min_price)
+            max_profit = max(profit, max_profit)
+
         return max_profit
-
-
-Time: O(N)
-Space : O(1)
-
