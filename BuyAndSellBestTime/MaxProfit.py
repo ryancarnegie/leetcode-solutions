b5c301a20856046c977efa6b02be5c4b16a9a2fe
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-28 22:42:56 -0800

    Update MaxProfit.py

--- BuyAndSellBestTime/MaxProfit.py
+++ BuyAndSellBestTime/MaxProfit.py
@@ -2,7 +2,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = 0
         for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
+            for j in range(i + 1, len(prices)):
                 profit = prices[j] - prices[i]
                 if profit > 0:
                     max_profit = max(max_profit, profit)
