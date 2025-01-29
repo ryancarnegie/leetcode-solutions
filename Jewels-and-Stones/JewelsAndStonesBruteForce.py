b27c4a2ef35bb5498ced80d2904a39c76fa1c7df
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-28 16:06:41 -0800

    Update JewelsAndStonesBruteForce.py

--- Jewels-and-Stones/JewelsAndStonesBruteForce.py
+++ Jewels-and-Stones/JewelsAndStonesBruteForce.py
@@ -8,3 +8,16 @@ class Solution:
 
 Time : O(M * N)
 Space : O(1)
+
+
+Outer loop (for stone in stones): 
+𝑁
+Inner loop (for jewel in jewels or if stone in jewels): 
+𝑀
+
+
+M⋅N because:
+M corresponds to jewels, which is the smaller collection in most cases, and
+N corresponds to stones, which is usually the larger collection.
+
+M⋅N emphasizes the relationship between the smaller and larger input sizes. It doesn't change the actual complexity.
