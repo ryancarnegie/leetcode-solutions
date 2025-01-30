7b42a98c54599d9ee5f80918f5a84e9f6c1f89b1
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-30 11:07:55 -0800

    Update JewelsAndStonesHashSet.py

--- Jewels-and-Stones/JewelsAndStonesHashSet.py
+++ Jewels-and-Stones/JewelsAndStonesHashSet.py
@@ -9,3 +9,16 @@ class Solution:
 
 Time: O(M + N)
 Space: O(N) because it is turning that string into a {set} of jewels. That set will hold the same amount of space as the stuff its storing. 
+
+    
+        # Modified vr
+        # You make s a set of jewels to enable efficient membership checking. Sets in Python provide 
+        # O(1) average-time complexity for lookups, which makes checking whether a stone is a jewel much faster 
+        # compared to iterating through a list of jewels (which would take O(M) for each lookup). 
+        # This optimization significantly improves performance.
+        count = 0
+        jewel_set = set(jewels)
+        for stone in stones:
+            if stone in jewel_set:
+                count += 1
+        return count
