933cd4f9ecab998955febabc10e30f03638bac1b
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-30 18:50:52 -0800

    Update JewelsAndStonesHashSet.py

--- Jewels-and-Stones/JewelsAndStonesHashSet.py
+++ Jewels-and-Stones/JewelsAndStonesHashSet.py
@@ -18,7 +18,6 @@ Space: O(N) because it is turning that string into a {set} of jewels. That set w
 # O(1) average-time complexity for lookups, which makes checking whether a stone is a jewel much faster 
 # compared to iterating through a list of jewels (which would take O(M) for each lookup). 
 # This optimization significantly improves performance.
-    
     count = 0
     jewels_set = set(jewels)
     for stone in stones:
