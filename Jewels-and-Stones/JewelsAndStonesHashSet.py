e11bded6ee87cfa04e1144064897000ae6feea57
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-01-30 18:54:00 -0800

    Update JewelsAndStonesHashSet.py

--- Jewels-and-Stones/JewelsAndStonesHashSet.py
+++ Jewels-and-Stones/JewelsAndStonesHashSet.py
@@ -1,14 +1,25 @@
 # Brute Force Solution
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for stone in stones:
+            if stone in jewels:
+                count += 1
+        return count
+    # Time: (M * N)
+    # Space: O(1)
+
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
         s = set(jewels)
         count = 0
         for stone in stones:
             if stone in s:
                 count += 1
         return count
-Time: O(M + N)
-Space: O(N) because it is turning that string into a {set} of jewels. That set will hold the same amount of space as the stuff its storing. 
+        Time: O(M + N)
+        Space: O(N) because it is turning that string into a {set} of jewels. That set will hold the same amount of space as the stuff its storing. 
 
 
 # Optimal Solution
@@ -17,9 +28,11 @@ Space: O(N) because it is turning that string into a {set} of jewels. That set w
 # O(1) average-time complexity for lookups, which makes checking whether a stone is a jewel much faster 
 # compared to iterating through a list of jewels (which would take O(M) for each lookup). 
 # This optimization significantly improves performance.
-    count = 0
-    jewels_set = set(jewels)
-    for stone in stones:
-        if stone in jewels_set:
-            count += 1
-    return count
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        jewels_set = set(jewels)
+        for stone in stones:
+            if stone in jewels_set:
+                count += 1
+        return count
