0aa5b23f9fbda901aa35a7052889c1ac89de5491
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-02-01 20:31:21 -0800

    Update JewelsAndStonesHashSet.py

--- Jewels-and-Stones/JewelsAndStonesHashSet.py
+++ Jewels-and-Stones/JewelsAndStonesHashSet.py
@@ -10,18 +10,6 @@ class Solution:
     # Space: O(1)
 
 
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        s = set(jewels)
-        count = 0
-        for stone in stones:
-            if stone in s:
-                count += 1
-        return count
-        Time: O(M + N)
-        Space: O(N) because it is turning that string into a {set} of jewels. That set will hold the same amount of space as the stuff its storing. 
-
-
 # Optimal Solution
 # Modified version
 # You make s a set of jewels to enable efficient membership checking. Sets in Python provide 
@@ -36,3 +24,5 @@ class Solution:
             if stone in jewels_set:
                 count += 1
         return count
+    Time: O(M + N)
+    Space: O(N) because it is turning that string into a {set} of jewels. That set will hold the same amount of space as the stuff its storing. 
