f12d8e843a1c22e69992aa245bc5c50564a44306
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-02-01 22:35:45 -0800

    Create TwoSumBruteForce.py

---
+++ TwoSum/TwoSumBruteForce.py
@@ -0,0 +1,10 @@
+# Brute Force Solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# Time : O(n^2)
+# Space : O(1)
