cb1abd7f8bf00e67798762c534881ece4518969e
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-02-08 22:40:31 -0800

    Update and rename GroupAnagramsOptimal.py to GroupAnagramsBruteForce.py

---
+++ Group-Anagrams/GroupAnagramsBruteForce.py
@@ -0,0 +1,28 @@
+# Brute Force Solutions
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for s in strs:
+            key = ''.join(sorted(s))
+            ans[key].append(s)
+        return list(ans.values())
+
+# Time: O(n)
+# Space: 
+
+
+# Explanation:
+# The defaultdict turns the if/else statement into a one liner.
+# If it is not a defaultdict, it would be:
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key in ans:
+                ans[key].append(s)
+            else:
+                ans[key] = [s]
+        return list(ans.values())
--- Group-Anagrams/GroupAnagramsOptimal.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = defaultdict(list)
-        for s in strs:
-            key = ''.join(sorted(s))
-            ans[key].append(s)
-          
-        return list(ans.values())
