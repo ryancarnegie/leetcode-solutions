38be9f5f64e3c2c0fd16b9dfa113edb000c31e35
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-02-12 22:56:09 -0800

    Update and rename TwoSumBruteForce.py to TwoSumTwoPointer.py

---
+++ TwoSumII/TwoSumTwoPointer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        l = 0
+        r = n - 1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum == target:
+                return [l + 1, r + 1]
+            elif sum < target:
+                l += 1
+            else:
+                r -= 1
+      
+        # Time: O(n)
+        # Space: O(1)
