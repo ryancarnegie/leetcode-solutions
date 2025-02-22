07ce81b50763845024794d2bfbf62ed88b7ec13a
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2025-02-22 10:50:54 -0800

    Create BaseballGame.py

---
+++ BaseballGame/BaseballGame.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for item in operations:
+            if item == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif item == 'D':
+                stack.append(stack[-1] * 2)
+            elif item == 'C':
+                stack.pop()
+            else:
+                stack.append(int(item))
+        return sum(stack)
+
+# Time: O(n)
+# Space: O(n)
